25c02ada5812382403771b23786d10cdfd842c48
Gringo <aryanatha@gmail.com>
2026-04-27 04:35:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-const tg = window.Telegram.WebApp;
-const API_BASE = "https://search.sigmaboy.us";
-
-const i18n = {
-    "daily": "Ежедневно",
-    "hourly": "Ежечасно",
-    "realtime": "В реальном времени",
-    "never": "Никогда",
-    "total_msgs": "сообщ.",
-    "topics_all": "Все разделы",
-    "topics_sel": "Разделов: "
-};
-
-tg.ready();
-tg.expand();
-
-let currentPreview = null;
-let userProfile = null;
-let sourcesCache = {}; // Глобальный кэш для хранения описаний
-// Глобальная переменная для ID пользователя (резервный вход)
-let globalUserId = new URLSearchParams(window.location.search).get('user_id');
-
-
 // Показ данных профиля (Личный кабинет)
 document.getElementById('settings-btn').addEventListener('click', () => {
     if (!userProfile) return tg.showAlert("Данные профиля еще не загружены");
