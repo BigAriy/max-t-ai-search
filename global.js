067d23cd8de0eee60f97145ac1b8fbe34c8c5cee
Gringo <aryanatha@gmail.com>
2026-04-27 03:42:47 +0200

    Create global.js

---
+++ global.js
@@ -0,0 +1,21 @@
+const tg = window.Telegram.WebApp;
+const API_BASE = "https://search.sigmaboy.us";
+
+const i18n = {
+    "daily": "Ежедневно",
+    "hourly": "Ежечасно",
+    "realtime": "В реальном времени",
+    "never": "Никогда",
+    "total_msgs": "сообщ.",
+    "topics_all": "Все разделы",
+    "topics_sel": "Разделов: "
+};
+
+tg.ready();
+tg.expand();
+
+let currentPreview = null;
+let userProfile = null;
+let sourcesCache = {}; // Глобальный кэш для хранения описаний
+// Глобальная переменная для ID пользователя (резервный вход)
+let globalUserId = new URLSearchParams(window.location.search).get('user_id');
